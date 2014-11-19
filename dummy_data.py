03cae2f68910048a209c783e263f94243ceaad4d
jbourdea <jbourdea@uoguelph.ca>
2014-11-19 12:34:14 -0500

    First checkin
    It's mostly temporary files

---
+++ dummy_data.py
@@ -0,0 +1,12 @@
+# Dummy data
+COMPANY = "Garden Fresh Box"
+
+PEOPLE = [
+        {'name': 'sstanton', 'title': 'Susan Stanton'},
+        {'name': 'bbarker', 'title': 'Bob Barker'},
+]
+
+SITE_MENU = [
+        {'href': '', 'title': 'Home'},
+        {'href': 'acme', 'title': COMPANY},
+]
